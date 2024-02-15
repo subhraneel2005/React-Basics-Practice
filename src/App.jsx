d9ad5cb0f8026af91d4f27962e3a9ab3fd6e8bd8
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-02-15 11:08:38 +0530

    Learned about Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import First from './First'
 import Second from './Second'
+import PropsPractice from './PropsPractice'
 
 
 
 function App() {
+
+  let user = {
+    username:'JeetChomuLord',
+    description:'As u can see i am not dead !!!',
+  }
   return (
     <div>
       <First/>
-      <Second/>
+      {/* <Second/> */}
+      <PropsPractice username="Chacha" />
+      <PropsPractice username="JeetChomuLord"/>
+      <PropsPractice username="Chachi" />
+      <PropsPractice/><PropsPractice/>
     </div>
   )
 }
