0b153a551198a70c68cc093e034276dab04e52e3
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-02-24 23:55:30 +0530

    #login done

--- src/contexts/BMI Calculator/Bmi.jsx
+++ src/contexts/BMI Calculator/Bmi.jsx
@@ -3,12 +3,25 @@ import React, { useState } from 'react'
 function Bmi() {
 
     const [bmi, setBmi] = useState(null);
+    const [weight, setWeight] =useState();
+    const [ height,setHeight] = useState();
+
+    const calculate = () => {
+
+    }
 
   return (
     <div className='h-screen w-full flex justify-center items-center'>
         <div className='bg-yellow-300 shadow-2xl rounded-lg p-8 flex justify-center items-center w-[400px] h-[450px]'>
             <div className='block'>
-                
+                <h1>BMI Calculator</h1>
+                <input type="text"
+                placeholder='Enter weight in kgs'
+                onChange={(e) => setWeight(e.target.value)}/>
+                <input type="text"
+                placeholder='Enter height in meters'
+                onChange={(e) => setHeight(e.target.value)}/>
+                <button onClick={calculate}>Calculate BMI</button>
             </div>
         </div>
     </div>
