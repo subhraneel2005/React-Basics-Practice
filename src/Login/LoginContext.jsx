793d654632430ac49da7e0ab66188153d3ccf7a9
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-02-24 22:51:07 +0530

    ui done

--- src/Login/LoginContext.jsx
+++ src/Login/LoginContext.jsx
@@ -9,15 +9,26 @@ function LoginContext() {
 
     const {setUser} = useContext(UseContext);
 
+    const handleClick = (e) => {
+        e.preventDefault();
+        setUser({username,email,password});
+    }
+
   return (
     <div className='h-screen w-full flex justify-center items-center'>
         <div className='w-[500px] h-[500px] rounded-e-xl bg-lime-500'>
             <div className='block'>
                 <h1></h1>
-                <input type="text" />
-                <input type="text" />
-                <input type="text" />
-                <button>Login</button>
+                <input placeholder='Username'
+                onChange={(e)=>setUsername(e.target.value)} value={username}
+                type="text" />
+                <input
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)} 
+                placeholder='Email' type="text" />
+                <input onChange={(e)=>setPassword(e.target.value)} value={password}
+                placeholder='Password' type="text" />
+                <button onClick={handleClick}>Login</button>
             </div>
         </div>
     </div>
