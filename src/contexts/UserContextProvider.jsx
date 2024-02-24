d86d31f6fdb08cf64e07716ac1e44213ea7b090d
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-02-24 23:30:26 +0530

    api fetched

--- src/contexts/UserContextProvider.jsx
+++ src/contexts/UserContextProvider.jsx
@@ -4,6 +4,29 @@ import UseContext from './UseContext'
 
 const UserContextProvider = ({children}) => {
 
+    //for pokemon data
+
+    const [pokemenData, setPokemenData] = useState([]);
+
+    const  getPokemons = () => {
+
+        const url = 'https://pokemon-api3.p.rapidapi.com/pokemon?name=%3CREQUIRED%3E';
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'c316315315msh0c888c6e83b886bp1d4caajsn67bd28eb77e9',
+                'X-RapidAPI-Host': 'pokemon-api3.p.rapidapi.com'
+            }
+        };
+
+        fetch(url,options)
+        .then(res => res.json())
+        .then(json => setPokemenData(json));
+    }
+
+
+    //for login page
+
     const[userData, setUserData] = React.useState({});
 
     //for theme changer
