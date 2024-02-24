ff38497bfe2f966a3b4039133d99fbcfc584d419
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-02-24 23:49:29 +0530

    #4

--- src/contexts/BMI Calculator/Bmi.jsx
+++ src/contexts/BMI Calculator/Bmi.jsx
@@ -7,7 +7,9 @@ function Bmi() {
   return (
     <div className='h-screen w-full flex justify-center items-center'>
         <div className='bg-yellow-300 shadow-2xl rounded-lg p-8 flex justify-center items-center w-[400px] h-[450px]'>
-            
+            <div className='block'>
+                
+            </div>
         </div>
     </div>
   )
