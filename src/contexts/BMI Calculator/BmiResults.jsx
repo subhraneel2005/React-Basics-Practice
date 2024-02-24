8fd652fc5ee23f60dca4dae74c5bfa6ef3b0c7ae
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-02-25 00:11:16 +0530

    bmi calculator done #2

--- src/contexts/BMI Calculator/BmiResults.jsx
+++ src/contexts/BMI Calculator/BmiResults.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 function BmiResults(props) {
   if(!props.bmi)
   {
-    return <h1>Please enter all details </h1>
+    return <h1 className='text-center'>Please enter all details </h1>
   }
   else
   {
-    <h1>BMI : {props.bmi}</h1>
+    <h1 className='text-center'>BMI : {props.bmi}</h1>
   }
 }
 
