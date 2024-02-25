d192178438021d110918e761ffaedbff5ff3c12d
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-02-25 13:05:45 +0530

    outer box ui done #1

--- src/contexts/SingleProductCOmponent/SingleProduct.jsx
+++ src/contexts/SingleProductCOmponent/SingleProduct.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function SingleProduct() {
   return (
-    <div>SingleProduct</div>
+    <div className='h-[500px] w-[500px] rounded-xl bg-slate-700 flex justify-center items-center'>
+        
+    </div>
   )
 }
 
