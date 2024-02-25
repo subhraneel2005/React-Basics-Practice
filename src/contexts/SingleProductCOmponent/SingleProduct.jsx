6a1eae67394f159d067b58f7e1f62dcf285fb7a7
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-02-25 13:08:08 +0530

    all done #2

--- src/contexts/SingleProductCOmponent/SingleProduct.jsx
+++ src/contexts/SingleProductCOmponent/SingleProduct.jsx
@@ -4,7 +4,7 @@ function SingleProduct() {
   return (
     <div className='h-[500px] w-[500px] rounded-xl bg-slate-700 flex justify-center items-center'>
         <div className='block'>
-            <div className='h-[250px] w-[250px] rounded-xl bg-slate-900'></div>
+            <div className='h-[300px] w-[350px] rounded-xl bg-slate-900'>Product img</div>
         </div>
     </div>
   )
