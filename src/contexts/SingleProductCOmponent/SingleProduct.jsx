14c1b7aa261168fefdca5313d4a3f4a5ee3b8559
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-02-25 13:07:20 +0530

    inner box done #2

--- src/contexts/SingleProductCOmponent/SingleProduct.jsx
+++ src/contexts/SingleProductCOmponent/SingleProduct.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function SingleProduct() {
   return (
     <div className='h-[500px] w-[500px] rounded-xl bg-slate-700 flex justify-center items-center'>
-        
+        <div className='block'>
+            <div className='h-[250px] w-[250px] rounded-xl bg-slate-900'></div>
+        </div>
     </div>
   )
 }
